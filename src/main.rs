529e5b737f9d9ec147f9afcfaec7743e2c34222c
ravinderj <ravindj@thoughtworks.com>
2020-11-13 20:57:31 +0530

    Read tags as cli arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use ec2_bridge::commands::ip;
 use rusoto_core::Region;
 use rusoto_ec2::Ec2Client;
+use structopt::StructOpt;
 
 fn main() {
+    let cli = Cli::from_args();
     let client = Ec2Client::new(Region::EuWest1);
-    ip::show_ips(client, vec!["airflow".to_string(), "sonar".to_string()]);
+    ip::show_ips(client, cli.tags);
+
+}
+
+#[derive(StructOpt, Debug)]
+struct Cli {
+    #[structopt(short, long)]
+    tags: Vec<String>
 }
